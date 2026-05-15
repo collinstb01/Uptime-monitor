5ea25bfdc9f3ce29b5fe3a3f0bf624350d3d7d12
TB <collinstb01@gmail.com>
2026-05-15 23:37:03 +0100

    Initial setup of models, migrations, mailables, jobs, and controllers for Uptime Monitor

---
+++ app/Http/Controllers/Monitor/MonitorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Monitor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MonitorController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function history($id)
+    {
+
+    }
+}
