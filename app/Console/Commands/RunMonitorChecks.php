1240528a0e6f0b71bfdf82496937c00eb9e78919
TB <collinstb01@gmail.com>
2026-05-15 23:24:24 +0100

    chore: initial laravel project setup

---
+++ app/Console/Commands/RunMonitorChecks.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:run-monitor-checks')]
+#[Description('Command description')]
+class RunMonitorChecks extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
